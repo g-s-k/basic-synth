cc3f1cbbe51be2a404db8db1dff00810e35947a2
George Kaplan <gkaplan@fearless.tech>
2021-09-10 17:56:41 -0400

    create amp envelope generator per voice

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::{
     mem, process,
     sync::mpsc::{self, Sender, TryRecvError},
     thread,
-    time::Duration,
 };
 
 use {
@@ -72,17 +71,18 @@ fn run_synth_bg() -> Sender<MidiMsg> {
     let (tx, rx) = mpsc::channel::<MidiMsg>();
 
     thread::spawn(move || {
-        let mut synth = Synth::new(16);
+        let mut synth = Synth::new(8);
         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
         let sink = Sink::try_new(&stream_handle).unwrap();
 
         loop {
             match rx.try_recv() {
                 Err(TryRecvError::Empty) => {
-                    let buffer: Vec<f32> = (0..BLOCK_SIZE).flat_map(|_| synth.next()).collect();
-                    sink.append(SamplesBuffer::new(1, SAMPLE_RATE, buffer));
-                    // wait half of a block so we don't get too far ahead
-                    thread::sleep(Duration::from_millis(500 / BLOCKS_PER_SECOND as u64));
+                    // don't get ahead of ourselves
+                    if sink.len() < 5 {
+                        let buffer: Vec<f32> = (0..BLOCK_SIZE).flat_map(|_| synth.next()).collect();
+                        sink.append(SamplesBuffer::new(1, SAMPLE_RATE, buffer));
+                    }
                 }
                 Err(TryRecvError::Disconnected) => {
                     panic!(
@@ -94,8 +94,7 @@ fn run_synth_bg() -> Sender<MidiMsg> {
                     ..
                 }) => {
                     if let Some(v) = synth.get_new_voice() {
-                        v.on = true;
-                        v.set_note(note);
+                        v.begin_note(note);
                     } else {
                         eprintln!(
                             "Out of voices. Note requested was {} with velocity {}",
@@ -108,7 +107,7 @@ fn run_synth_bg() -> Sender<MidiMsg> {
                     ..
                 }) => {
                     if let Some(v) = synth.get_playing_voice(note) {
-                        v.on = false;
+                        v.end_note();
                     } else {
                         eprintln!(
                             "Expected a voice playing note {} but could not find one",
@@ -171,14 +170,22 @@ struct Voice {
     on: bool,
     note: u8,
     oscillators: [Oscillator; 3],
+    amp_eg: Adsr,
 }
 
 impl Voice {
-    fn set_note(&mut self, new_note: u8) {
+    fn begin_note(&mut self, new_note: u8) {
+        self.on = true;
         self.note = new_note;
         for osc in &mut self.oscillators {
             osc.current_freq = (2_f32).powf((self.note as i16 - 69) as f32 / 12.0) * 440.0;
         }
+        self.amp_eg.segment = AdsrSegment::Attack(0.0);
+    }
+
+    fn end_note(&mut self) {
+        self.on = false;
+        self.amp_eg.segment = AdsrSegment::Release(0.0);
     }
 }
 
@@ -186,12 +193,9 @@ impl Iterator for Voice {
     type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // TODO replace with EG
-        if !self.on {
-            return Some(0.0);
-        }
-
-        Some(self.oscillators.iter_mut().flat_map(|o| o.next()).sum())
+        let osc_mix: f32 = self.oscillators.iter_mut().flat_map(|o| o.next()).sum();
+        let amp_volume = self.amp_eg.next().unwrap();
+        Some(osc_mix * amp_volume)
     }
 }
 
@@ -208,4 +212,79 @@ impl Iterator for Oscillator {
         let next_phase = (self.current_phase + TAU * self.current_freq / SAMPLE_RATE as f32) % TAU;
         Some(mem::replace(&mut self.current_phase, next_phase).sin())
     }
+}
+
+#[derive(Debug)]
+struct Adsr {
+    segment: AdsrSegment,
+    attack_time: f32,
+    decay_time: f32,
+    sustain_amount: f32,
+    release_time: f32,
+}
+
+impl Default for Adsr {
+    fn default() -> Self { 
+        Self {
+            segment: AdsrSegment::Off,
+            attack_time: 0.05,
+            decay_time: 0.25,
+            sustain_amount: 0.5,
+            release_time: 0.5,
+        }
+    }
+}
+
+impl Iterator for Adsr {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(match self.segment {
+            AdsrSegment::Off => 0.0,
+            AdsrSegment::Attack(amt) if amt >= 1.0 => {
+                self.segment = AdsrSegment::Decay(0.0);
+                1.0
+            }
+            AdsrSegment::Attack(current_amt) => {
+                self.segment = AdsrSegment::Attack(current_amt + self.attack_time / SAMPLE_RATE as f32);
+                // (y2 - y1) = m * (x2 - x1)
+                // (y2 - 0.0) = (1.0 / attack) * (x2 - 0.0)
+                // y2 = x2 / attack
+                current_amt / self.attack_time
+            }
+            AdsrSegment::Decay(amt) if amt >= 1.0 => {
+                self.segment = AdsrSegment::Sustain;
+                self.sustain_amount
+            }
+            AdsrSegment::Decay(current_amt) => {
+                self.segment = AdsrSegment::Decay(current_amt + self.decay_time / SAMPLE_RATE as f32);
+                // (y2 - y1) = m * (x2 - x1)
+                // (y2 - 1.0) = (1 - sustain) / 1.0 * (x2 - 0.0)
+                // y2 = (1 - sustain) * x2 + 1.0
+                (1.0 - self.sustain_amount) * current_amt + 1.0
+            }
+            AdsrSegment::Sustain => self.sustain_amount,
+            AdsrSegment::Release(amt) if amt >= 1.0 => {
+                self.segment = AdsrSegment::Off;
+                0.0
+            }
+            AdsrSegment::Release(current_amt) => {
+                self.segment = AdsrSegment::Release(current_amt + self.release_time / SAMPLE_RATE as f32);
+                // (y2 - y1) = m * (x2 - x1)
+                // (y2 - sustain) = (-sustain) * (x2 - 0.0)
+                // y2 = (-sustain) * x2 + sustain
+                // y2 = sustain * (1.0 - x2)
+                self.sustain_amount * (1.0 - current_amt)
+            }
+        })
+    }
+}
+
+#[derive(Debug)]
+enum AdsrSegment {
+    Off,
+    Attack(f32),
+    Decay(f32),
+    Sustain,
+    Release(f32),
 }
